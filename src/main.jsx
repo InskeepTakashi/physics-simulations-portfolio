2936ff5c46aa908d8f91c9c7bc15236cd0d57e0a
Takashi Inskeep <takashiinskeep@Takashis-iMac.localdomain>
2025-08-16 01:15:47 -0400

    Made title work properly

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Head from 'next/head'
 import Bounce from './Bounce.jsx'
 import Orbit from './Orbit.jsx'
 import Ring from './Ring.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Head>
-      <title>Physics Simulations Portfolio</title>
-    </Head>
-    <h1>Takashi Inskeep</h1>
+    <h1>Physics Simulations Portfolio</h1>
+    <hr />
+    <h2>Takashi Inskeep</h2>
     <Bounce />
     <Orbit />
     <Ring />
