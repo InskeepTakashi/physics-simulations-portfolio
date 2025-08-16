5ce54d26b4c64d53b859913d985fe1d37b12309d
Takashi Inskeep <takashiinskeep@Takashis-iMac.localdomain>
2025-08-16 01:04:35 -0400

    Added title

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Head from 'next/head'
 import Bounce from './Bounce.jsx'
 import Orbit from './Orbit.jsx'
 import Ring from './Ring.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Head>
+      <title>Physics Simulations Portfolio</title>
+    </Head>
     <h1>Takashi Inskeep</h1>
     <Bounce />
     <Orbit />
